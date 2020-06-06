a5c94391c28dc992ba759de41ca26417a8b42d4b
AlexDeathway <alexdeathway@gmail.com>
2020-06-06 21:41:40 +0530

    serializers added

--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+#REST framework imports
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+#importing serializers
+from .serializers import TaskSerializer
+
 # Create your views here.
+@api_view(['get'])
 def apioverview(request):
-    return JsonResponse("Api Catch endpoint",safe=False)
+    api_urls={
+    'list':'/task-list/',
+    }
+    return Response(api_urls)
